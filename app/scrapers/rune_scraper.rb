5c1df7be7e3ff254b59c29176d4d9a33813939d8
Chris Hulton <chrishulton@gmail.com>
2016-05-22 15:58:21 -0400

    Adds rune odds

--- app/scrapers/rune_scraper.rb
+++ app/scrapers/rune_scraper.rb
@@ -9,4 +9,22 @@ rune_table = doc.xpath('//table[@cellpadding=5]')
 
 ArreatScraper::parse_item_table(rune_table, rune_property_keys, runes)
 
+rune_drop_odds = {}
+
+rune_odds_doc = Nokogiri::HTML(open("./app/fixtures/rune_odds.html"))
+rune_odds_table = rune_odds_doc.xpath('//table/tbody')
+
+rune_odds_table.xpath('tr').each_with_index do |row, row_index|
+  next if row_index == 0
+
+  rune_name = row.xpath('td')[0].content.squish.match(/(\w+) Rune/)[1]
+  rune_odds = row.xpath('td')[2].content.squish
+
+  rune_drop_odds[rune_name] = rune_odds
+end
+
+runes.each do |rune|
+  rune["odds"] = rune_drop_odds[rune["name"]]
+end
+
 ArreatScraper::json_to_file("runes", runes)
